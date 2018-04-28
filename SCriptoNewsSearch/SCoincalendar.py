0ee5e6a881ad56dbe47ea0be4130e8e3e3d9a7c8
manh <manh@manh-Vostro-1440>
2018-04-29 01:41:34 +0700

    Modify Code
    Change file path to relative to work on different computer

--- SCriptoNewsSearch/SCoincalendar.py
+++ SCriptoNewsSearch/SCoincalendar.py
@@ -17,16 +17,22 @@ logging.basicConfig(filename='programLog.txt', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.DEBUG)
 logging.debug('Start of code')
-vietNewsShelf = shelve.open(os.path.join('C:\\001MyPythonScript\\src\\SNewsSearch','SNewsSearch'))
+
+cur_dir = os.getcwd()
+file_path = os.path.join(cur_dir,'DB','SNewsSearch')
+vietNewsShelf = shelve.open(file_path)
 list_keywords = []
 for value in vietNewsShelf.values():
     list_keywords.append(''.join(value).lower())
 url = 'http://www.coincalendar.info'              # starting url
-os.makedirs('C:/001MyPythonScript/src/SNewsSearch', exist_ok=True)   # store list in ./SManga
-with open('C:/001MyPythonScript/src/SNewsSearch/OldLink.txt','a+') as txt_OldLink:
+file_path = os.path.join(cur_dir)
+os.makedirs(file_path, exist_ok=True)   # store list in ./SManga
+file_path = os.path.join(cur_dir,'DB','OldLink.txt')
+with open(file_path,'a+') as txt_OldLink:
     pass
 txt_OldLink.close()
-with open('C:/001MyPythonScript/src/SNewsSearch/OldLink.txt','r+') as txt_OldLink:
+file_path = os.path.join(cur_dir,'DB','OldLink.txt')
+with open(file_path,'r+') as txt_OldLink:
     old_links = txt_OldLink.read()
     logging.debug(str(old_links))
     old_links = old_links.split('\n')
@@ -61,10 +67,12 @@ for each_keyword in list_keywords:
         if re_search is not None:
             # Check link in database
             if result_link not in old_links:
-                with open('C:/001MyPythonScript/src/SNewsSearch/OldLink.txt','a+') as txt_OldLink:
+                file_path = os.path.join(cur_dir,'DB','OldLink.txt')
+                with open(file_path,'a+') as txt_OldLink:
                     txt_OldLink.write(result_link + '\n')
                 txt_OldLink.close()
                 user_command = input('Search {} has new link: {}.\n Open link Y/Default No\n'.format(each_keyword, result_link))
                 if user_command.lower() == 'y':
                     webbrowser.open(result_link)
-    print('All result of keyword {} in {} is checked. Press any key to quit\n'.format(each_keyword, url))
+    print('All result of keyword {} in {} is checked.\n'.format(each_keyword, url))
+print('All keywords are checked. Program end\n')
